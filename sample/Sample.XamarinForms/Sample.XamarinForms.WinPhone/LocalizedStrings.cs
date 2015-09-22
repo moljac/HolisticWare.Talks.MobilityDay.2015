b784e123bbae304d16c31667773a1102e800d995
moljac Miljenko Cvjetko) <mcvjetko@holisticware.net>
2015-09-22 22:24:46 +0200

    Xamarin.forms sample

---
+++ sample/Sample.XamarinForms/Sample.XamarinForms.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Sample.XamarinForms.WinPhone.Resources;
+
+namespace Sample.XamarinForms.WinPhone
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
