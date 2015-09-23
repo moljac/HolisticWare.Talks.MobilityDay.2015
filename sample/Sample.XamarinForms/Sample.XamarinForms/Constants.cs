9d8b142189b72361ae42755024cdb843d5a87f7f
moljac Miljenko Cvjetko) <mcvjetko@holisticware.net>
2015-09-23 13:05:28 +0200

    artwork, presentations-keynote

---
+++ sample/Sample.XamarinForms/Sample.XamarinForms/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sample.XamarinForms
+{
+	public static class Constants
+	{
+		// Make sure to update this string with your App API key from http://insights.xamarin.com
+		public const string INSIGHTS_API_KEY = "82cfb6ddcd406068c713d90cfafef14aa05d5dcf";
+	}
+}
+
